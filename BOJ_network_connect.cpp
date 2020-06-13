dcafd936baae1101fd22bfaf1173d901f99ffa77
zzid <931010dy@gmail.com>
2020-06-13 22:31:43 +0900

    2020_06_13

--- BOJ_network_connect.cpp
+++ BOJ_network_connect.cpp
@@ -1,29 +1,51 @@
 // https://www.acmicpc.net/problem/1922
 /**
- *  
- *  kruskal algorithm 
- * 
+ *
+ *  kruskal algorithm (MST)
+ *
  **/
 #include <bits/stdc++.h>
 using namespace std;
 int parent[1001];
-int n,m;
+int n,m,answer=0;
+bool chk;
+typedef struct kruskal{
+    int from;
+    int to;
+    int cost;
+}ks;
+vector<ks> edge;
+bool comp(ks a, ks b){
+    return a.cost < b.cost;
+}
 int Find(int a){
     if(a == parent[a]) return a;
     return parent[a] = Find(parent[a]);
 }
 void Union(int a, int b){
+    chk = false;
     a = Find(a);
     b = Find(b);
-    if(a < b) parent[b] = a;
-    else parent[a] = b;
+    if(a == b) return;
+    parent[a] = b;
+    chk = true;
 }
 int main(){
     scanf("%d", &n);
     scanf("%d", &m);
+    for(int i=1; i<=n; i++) parent[i] = i;
     for(int i=0; i<m; i++){
-        int a,b,c;
-        scanf("%d %d %d", &a, &b, &c);
+        // int a,b,c;
+        ks kr;
+        scanf("%d %d %d", &kr.from, &kr.to, &kr.cost);
+        edge.push_back(kr);
     }
+    sort(edge.begin(), edge.end(), comp);
+    for(auto i : edge){
+        Union(i.from, i.to);
+        if(chk) answer += i.cost;
+    }
+    printf("%d", answer);
+
     return 0;
 }
