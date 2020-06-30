f9eae842b236074064f6b135b2c7a46f4720ee3c
zzid <931010dy@gmail.com>
2020-06-30 19:13:30 +0900

    2020_06_30 (incomplete)

--- 2020_06_30/BOJ_greedy_panda.cpp
+++ 2020_06_30/BOJ_greedy_panda.cpp
@@ -8,19 +8,18 @@ int forest[500][500];
 bool visited[500][500];
 int dp[500][500];
 int dfs(int y, int x, int days){
+    if(visited[y][x]) return 0;
+    visited[y][x] = true;
     if(dp[y][x] != 0) return dp[y][x];
-    int& ret = dp[y][x];
     for(int i=0; i<4; i++){
         int ny = y + dy[i], nx = x +dx[i];
         if(ny >= n || ny < 0 || nx >= n || nx < 0) continue;
-        if(visited[ny][nx]) continue;
         if(forest[ny][nx] > forest[y][x]){
-            visited[ny][nx] = true;
-            ret = max(ret, dp[y][x] + dfs(ny,nx,days+1));
+            dp[ny][nx] = dp[ny][nx] + dfs(ny,nx,days+1);
             visited[ny][nx] = false;
         }
+        else return days;
     }
-    return 1;
 }
 int main(){
     cin >> n;
@@ -32,16 +31,13 @@ int main(){
     }
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            visited[i][j] = true;
             dfs(i,j,1);
         }
     }
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             answer = answer > dp[i][j] ? answer : dp[i][j];
-            cout << dp[i][j] << " ";
         }
-        cout << endl;
     }
     cout << answer << "\n";
     return 0;
