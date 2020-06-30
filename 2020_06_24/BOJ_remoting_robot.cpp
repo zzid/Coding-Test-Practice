b920967a6868e376cae4f5f624d2603d1037f84d
zzid <931010dy@gmail.com>
2020-06-30 19:36:36 +0900

    2020_06_30 (incomplete)

--- 2020_06_24/BOJ_remoting_robot.cpp
+++ 2020_06_24/BOJ_remoting_robot.cpp
@@ -1,26 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,answer =INT_MIN;
-typedef pair<pair<int, int>,int> pp;
+int n, answer =INT_MIN;
 const int dy[4] ={1,-1,0,0};
 const int dx[4] ={0,0,1,-1};
 int forest[500][500];
 bool visited[500][500];
 int dp[500][500];
-int dfs(int y, int x, int days){
+void prt(){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int dfs(int y, int x){
     if(dp[y][x] != 0) return dp[y][x];
-    int& ret = dp[y][x];
+    bool change = false;
     for(int i=0; i<4; i++){
         int ny = y + dy[i], nx = x +dx[i];
         if(ny >= n || ny < 0 || nx >= n || nx < 0) continue;
         if(visited[ny][nx]) continue;
         if(forest[ny][nx] > forest[y][x]){
+            change = true;
             visited[ny][nx] = true;
-            ret = max(ret, dp[y][x] + dfs(ny,nx,days+1));
+            dp[y][x] = max(dp[y][x], dfs(ny,nx) + 1 );
             visited[ny][nx] = false;
         }
     }
-    return 1;
+    if(!change) return dp[y][x] = 1;
 }
 int main(){
     cin >> n;
@@ -32,8 +40,7 @@ int main(){
     }
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            visited[i][j] = true;
-            dfs(i,j,1);
+            dfs(i,j);
         }
     }
     for(int i=0; i<n; i++){
@@ -41,7 +48,7 @@ int main(){
             answer = answer > dp[i][j] ? answer : dp[i][j];
             cout << dp[i][j] << " ";
         }
-        cout << endl;
+        cout <<endl;
     }
     cout << answer << "\n";
     return 0;
