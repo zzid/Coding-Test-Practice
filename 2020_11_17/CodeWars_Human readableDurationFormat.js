d7226ccd567f00979a33ba425bb9da0abebcad4e
zzid <931010dy@gmail.com>
2020-11-17 16:11:30 +0900

    2020_11_16 (modified, more readable)

--- 2020_11_17/CodeWars_Human readableDurationFormat.js
+++ 2020_11_17/CodeWars_Human readableDurationFormat.js
@@ -7,10 +7,14 @@ function formatDuration(seconds) {
     yy = parseInt(parseInt(parseInt(seconds / 3600) / 24) / 365);
   let times = [yy, dd, hh, mm, ss];
   console.log(times);
-  let domain = { 0: "year", 1: "day", 2: "hour", 3: "minute", 4: "second" };
+  // let domain = { 0: "year", 1: "day", 2: "hour", 3: "minute", 4: "second" };
+  // CHANGED >>
+  let domain = ["year", "day", "hour", "minute", "second"];
   let domainAdded = times.reduce((acc, e, i) => {
-    let str = "";
-    str += e !== 0 ? `${e} ${domain[i]}` : "";
+    // let str = "";
+    // str += e !== 0 ? `${e} ${domain[i]}` : "";
+    // CHANGED >>
+    let str = e !== 0 ? `${e} ${domain[i]}` : "";
     if (e !== 0) {
       str += e > 1 ? "s" : "";
       acc.push(str);
@@ -18,14 +22,16 @@ function formatDuration(seconds) {
     return acc;
   }, []);
   console.log(domainAdded);
-  if (domainAdded.length === 1) return domainAdded[0].trim();
+  if (domainAdded.length === 1) return domainAdded[0];
   let ret = domainAdded.reduce((acc, e, i) => {
-    if (i === domainAdded.length - 1) {
-      acc += " and " + e;
-    } else if (i === 0) acc += e;
+    // if (i === domainAdded.length - 1) {
+    //   acc += " and " + e;
+    // } else if (i === 0) acc += e;
+    // CHANGED >>
+    if (i === 0) acc += e;
+    else if (i === domainAdded.length - 1) acc += " and " + e;
     else acc += ", " + e;
     return acc;
   }, "");
-  console.log(ret.trim());
-  return ret.trim();
+  return ret;
 }
