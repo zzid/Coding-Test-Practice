378721e979c2dc2a9e99ecd17e8caf9ab301199b
zzid <931010dy@gmail.com>
2020-11-26 11:24:31 +0900

    2020_11_26

--- 2020_11_25/CodeWars_Maximum_subarray_sum.js
+++ 2020_11_25/CodeWars_Maximum_subarray_sum.js
@@ -1,14 +1,29 @@
-var maxSequence = function (arr) {
-  if (arr.length === 0) return 0;
-  let retArr = arr.reduce((acc, e, i) => {
-    if (acc.length) {
-      let lastAndNow = acc[acc.length - 1] + e;
-      acc.push(lastAndNow > e ? lastAndNow : e);
-    } else {
-      acc.push(e);
+function validParentheses(parens) {
+  let ret = true;
+  console.log(parens);
+  let lefts = parens.split('').reduce((acc, e, i) => {
+    if (ret) {
+      if (i === 0) {
+        // initial
+        acc.push(e);
+        return acc;
+      } else {
+        if (e === '(') {
+          acc.push(e);
+          return acc;
+        } else {
+          // if ')'
+          if (acc.length === 0) {
+            ret = false;
+          } else {
+            acc.pop();
+            return acc;
+          }
+        }
+      }
     }
     return acc;
   }, []);
-  let maxVal = Math.max(...retArr);
-  return maxVal < 0 ? 0 : maxVal;
-};
+  if (lefts.length) ret = false;
+  return ret;
+}
