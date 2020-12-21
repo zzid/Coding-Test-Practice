dafd70f7f6aeae26904911a8ae80394fc73a7a7a
zzid <931010dy@gmail.com>
2020-12-21 14:07:16 +0900

    2020_12_21

--- 2020_12_21/CodeWars_Snail.js
+++ 2020_12_21/CodeWars_Snail.js
@@ -36,3 +36,14 @@ const snail = (array) => {
   }
   return result;
 };
+
+
+// function snail(array) {
+//   var vector = [];
+//   while (array.length) {
+//     vector.push(...array.shift());
+//     array.map(row => vector.push(row.pop()));
+//     array.reverse().map(row => row.reverse());
+//   }
+//   return vector;
+// }
