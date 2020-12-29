aaafbc1f8a44b21e7c429f834da5a1cd954724a3
zzid <931010dy@gmail.com>
2020-12-29 10:31:33 +0900

    2020_12_29

---
+++ 2020_12_29/Programmers_ProductOfConsecutiveFibNumbers.js
@@ -0,0 +1,12 @@
+function productFib(prod) {
+  let fibNums = [0, 1];
+  let curIdx = 2;
+  let curProd = 0;
+  while (curProd < prod) {
+    fibNums[curIdx] = fibNums[curIdx - 1] + fibNums[curIdx - 2];
+    curProd = fibNums[curIdx] * fibNums[curIdx - 1];
+    if (curProd === prod) return [fibNums[curIdx - 1], fibNums[curIdx], true];
+    else if (curProd > prod) return [fibNums[curIdx - 1], fibNums[curIdx], false];
+    curIdx++;
+  }
+}
